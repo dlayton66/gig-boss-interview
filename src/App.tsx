80cbb080ea1bc2b9485876c78763f4e9e68657a5
Drew Layton <dlayton66@gmail.com>
2024-03-29 00:37:47 -0500

    add RenderArea folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import data from "./data/income_data_2023_24.json";
 import BandDropdown from "./components/BandDropdown.tsx";
-import RenderArea from "./components/RenderArea.tsx";
+import RenderArea from "./components/RenderArea/RenderArea.tsx";
 import ExportData from "./components/ExportData.tsx";
 import Band from "./interfaces/Band.ts";
 import { useState } from "react";
@@ -11,22 +11,19 @@ function App() {
   const [selectedBandName, setSelectedBandName] = useState("");
   const [isIncomeByMusician, setIsIncomeByMusician] = useState(false);
 
-  
   const receiveSelection = (selection: string) => {
     setSelectedBandName(selection);
   };
   const toggleFilter = () => {
     setIsIncomeByMusician(!isIncomeByMusician);
-  }
-  
-  const selectedBand = get_band_by_name(selectedBandName)
+  };
+
+  const selectedBand = get_band_by_name(selectedBandName);
   const get_band_names = data.bands.map((band: Band) => band.band_name);
   const total_income = data.bands.reduce(
     (accumulator, band) => accumulator + get_band_income(band),
     0
   );
-
-
 
   return (
     <div className="app">
@@ -34,7 +31,12 @@ function App() {
         Total Income for 2023-2024: <span>${total_income}</span>
       </div>
       <BandDropdown band_names={get_band_names} onSelect={receiveSelection} />
-      <button className={isIncomeByMusician ? "active" : ""} onClick={toggleFilter}>Income by Musician</button>
+      <button
+        className={isIncomeByMusician ? "active" : ""}
+        onClick={toggleFilter}
+      >
+        Income by Musician
+      </button>
       <ExportData />
       <RenderArea band={selectedBand} isIncomeByMusician={isIncomeByMusician} />
     </div>
--- src/components/RenderArea/RenderArea.tsx
+++ src/components/RenderArea/RenderArea.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import "../components/RenderArea.css";
-import Band from "../interfaces/Band";
+import "./RenderArea.css";
+import Band from "../../interfaces/Band";
 import {
   get_all_members_sorted,
   get_band_income,
   get_band_members_sorted,
   get_my_income,
-} from "../utils/BandUtils";
-import { Member } from "../interfaces/Member";
+} from "../../utils/BandUtils";
+import { Member } from "../../interfaces/Member";
 
 interface Props {
   band: Band | undefined;
@@ -48,9 +48,7 @@ function RenderArea({ band, isIncomeByMusician }: Props) {
   }
 
   const applyThresholdColor = (income: number) => {
-    return income >= 600 && dropdown === "All Members"
-      ? "over-threshold"
-      : "";
+    return income >= 600 && dropdown === "All Members" ? "over-threshold" : "";
   };
 
   return (
@@ -97,7 +95,6 @@ function RenderArea({ band, isIncomeByMusician }: Props) {
             <div className="divider"></div>
             <div className="bottom-container">
               {filtered_members
-                .filter((member) => member.name != "You")
                 .map((member) => (
                   <div className={applyThresholdColor(member.income)}>
                     {member.name}: ${member.income}
